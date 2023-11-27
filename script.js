859e997d34c90af14523ba998ffe28ac54a22d36
annisssimo <anyalar04@gmail.com>
2023-11-28 00:33:05 +0300

    feat: add ability to change grid size

--- script.js
+++ script.js
@@ -1,12 +1,41 @@
 
 const squareContainer = document.querySelector('.square-container');
+const changeNumberButton = document.querySelector('button');
+changeNumberButton.addEventListener('click', () => {
+    askUserNumber();
+});
 
-let n = 16 * 16;
-for (let i = 0; i < n; i++) {
-const innerSquare = document.createElement('div');
-innerSquare.classList.add('inner-square');
-squareContainer.appendChild(innerSquare);
-innerSquare.addEventListener('mouseover', 
-  e => e.target.classList.add('my-colour-class')
-)
+let userNumber = 16;
+
+function askUserNumber () {
+    const userNumber = prompt('Insert a number of squares per side (should be less than 100): ');
+    if (userNumber > 100 || userNumber <= 0 || isNaN(userNumber)) {
+        alert("Неверный ввод");
+        return;
+    }
+    calculateSquareSide(userNumber);
+    createGrid(userNumber);
 }
+
+
+function calculateSquareSide(userNumber) {
+    const squareSide = 480 / userNumber;
+    return squareSide;
+}
+
+
+function createGrid(userNumber) {
+    squareContainer.innerHTML = '';
+    const squareSide = calculateSquareSide(userNumber);
+    for (let i = 0; i < userNumber * userNumber; i++) {
+    const innerSquare = document.createElement('div');
+    innerSquare.style.cssText = `height: ${squareSide}px; width: ${squareSide}px;`;
+    innerSquare.classList.add('inner-square');
+    squareContainer.appendChild(innerSquare);
+    innerSquare.addEventListener('mouseover', 
+    e => e.target.classList.add('my-colour-class')
+    );
+    }
+}
+
+createGrid(userNumber);
