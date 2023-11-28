d02a2df62146fe7cbce1b8f8bea477fc95b4bfbf
annisssimo <anyalar04@gmail.com>
2023-11-28 20:36:34 +0300

    feat: add ability to change color

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
 
+let userNumber = 16;
+let isClicked = false;
+let selectedColor = 'pink';
+
 const squareContainer = document.querySelector('.square-container');
-const changeNumberButton = document.querySelector('button');
+const changeNumberButton = document.querySelector('#change-number-button');
+const changeModeButton = document.querySelector('#change-mode-button');
+const chooseColorButton = document.querySelector('#colorPicker');
+const clearButton = document.querySelector('#clear-button');
+
+clearButton.addEventListener('click', () => {
+   clearSquareContainer();
+});
+
+chooseColorButton.addEventListener('input', () => {
+    selectedColor = colorPicker.value;
+});
+
 changeNumberButton.addEventListener('click', () => {
     askUserNumber();
 });
 
-let userNumber = 16;
+
+changeModeButton.addEventListener('click', () => {
+    if (!isClicked) {
+        changeModeButton.textContent = 'Multicolor';
+        changeModeButton.style.backgroundColor = 'rgb(240, 110, 255)';
+        createMultiColorGrid(userNumber);    
+    }
+
+    else {
+        changeModeButton.textContent = 'Single color';
+        changeModeButton.style.backgroundColor = 'aqua';
+        createSingleColorGrid(userNumber);
+    }
+    isClicked = !isClicked;
+});
 
 function askUserNumber () {
     const userNumber = prompt('Insert a number of squares per side (should be less than 100): ');
@@ -14,7 +44,8 @@ function askUserNumber () {
         return;
     }
     calculateSquareSide(userNumber);
-    createGrid(userNumber);
+    if(changeModeButton.textContent === 'Multicolor') createMultiColorGrid(userNumber);
+    else if (changeModeButton.textContent === 'Single color') createSingleColorGrid(userNumber);
 }
 
 
@@ -24,18 +55,52 @@ function calculateSquareSide(userNumber) {
 }
 
 
-function createGrid(userNumber) {
+function createSingleColorGrid(userNumber) {
     squareContainer.innerHTML = '';
     const squareSide = calculateSquareSide(userNumber);
     for (let i = 0; i < userNumber * userNumber; i++) {
-    const innerSquare = document.createElement('div');
-    innerSquare.style.cssText = `height: ${squareSide}px; width: ${squareSide}px;`;
-    innerSquare.classList.add('inner-square');
-    squareContainer.appendChild(innerSquare);
-    innerSquare.addEventListener('mouseover', 
-    e => e.target.classList.add('my-colour-class')
-    );
+        const innerSquare = document.createElement('div');
+        innerSquare.style.cssText = `height: ${squareSide}px; width: ${squareSide}px;`;
+        innerSquare.classList.add('inner-square');
+        squareContainer.appendChild(innerSquare);
+        innerSquare.addEventListener('mousemove', () => innerSquare.style.backgroundColor = selectedColor);
     }
 }
 
-createGrid(userNumber);
+
+function createMultiColorGrid(userNumber) {
+    squareContainer.innerHTML = '';
+    const squareSide = calculateSquareSide(userNumber);
+    
+    for (let i = 0; i < userNumber * userNumber; i++) {
+        const innerSquare = document.createElement('div');
+        innerSquare.style.cssText = `height: ${squareSide}px; width: ${squareSide}px;`;
+        innerSquare.classList.add('inner-square');
+        squareContainer.appendChild(innerSquare);
+       
+        let hasBeenColored = false;
+       
+        innerSquare.addEventListener('mousemove', () => {
+            if(!hasBeenColored) {
+            innerSquare.style.backgroundColor = chooseRandomColor();
+            hasBeenColored = true;
+            }
+        });
+    }
+}
+
+function randomInteger(min, max) {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+function chooseRandomColor() {
+    const arrayRandomNumbers = [];
+        for (let i = 0; i < 3; i++) {
+            const randomNumber = randomInteger(0, 255);
+            arrayRandomNumbers.push(randomNumber);
+        }
+    return "rgb("+ arrayRandomNumbers.join() + ")";
+}
+
+createSingleColorGrid(userNumber);
